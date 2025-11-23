b5957df11c5ed2017e26c98e4070688c45f63e98
Guilherme <61890289+Guilhermeb-Ferrarezi@users.noreply.github.com>
2025-11-23 19:23:06 -0300

    Add files via upload

---
+++ Site com banco dee dados/Frontend/script.js
@@ -0,0 +1,18 @@
+const formcriar = document.getElementById("formCriar")
+
+formcriar.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const nome_exibicao = document.getElementById('nome_exibicao').value;
+    const usuario = document.getElementById('usuario').value;
+    const senha = document.getElementById('senha').value;
+
+    const resposta = await fetch("http://127.0.0.1:3000/criar", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ nome_exibicao, usuario, senha })
+    });
+
+    const data = await resposta.json();
+    alert(data.msg);
+});
